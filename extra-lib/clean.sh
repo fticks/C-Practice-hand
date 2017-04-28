dc50de7063f0c52490db1085d8b161efba576280
Hosea <hhcjingruoxuanjian@163.com>
2017-04-28 21:46:51 +0800

    modify .sh and rename readme.md

--- extra-lib/clean.sh
+++ extra-lib/clean.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 rm -rf *.h *.a
+
+# make clean for all lib
+make -C apue.3e/lib/ clean
+make -C unpv13e/lib/ clean
+make -C unpv13e/libfree/ clean
+rm -rf unpv13e/*.a
+rm -rf unpv13e/config.log                                                   
+make -C unpv22e/lib/ clean
+rm -rf unpv22e/*.a
